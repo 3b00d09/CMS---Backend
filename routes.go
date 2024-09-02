540976cfa2404686d74cfc979ba9c2a6dedb0aef
3b00d09 <bd315977@gmail.com>
2024-09-02 17:18:23 +0100

    deplyed to flyio and added config files

--- routes.go
+++ routes.go
@@ -8,6 +8,9 @@ import (
 
 func SetupRoutes() *fiber.App {
 	router := fiber.New()
+	router.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
 	router.Post("/login", handlers.Login)
 	router.Post("/register", handlers.Register)
 	router.Get("/logout", handlers.Logout)
